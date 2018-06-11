787ba67e0729eb23baade51cb178c26c0614f43f
Tianhang Liu <tianhanl@usc.edu>
2018-06-11 14:37:22 +0800

    add analysis tools

--- src/util/analysis.js
+++ src/util/analysis.js
@@ -1,3 +1,54 @@
 const csvParser = require('./csvParser');
+const fs = require('fs');
 
 const data = csvParser.parseLocalCSVSync('../data/titanic-train.csv');
+
+const relevantKeys = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Embarked'];
+
+const targetKey = 'Survived';
+
+const defaultValueFilter = value => value;
+
+const aggregateData = (
+  data,
+  relevantKeys,
+  targetKey,
+  targetKeyValue,
+  valueFilter = defaultValueFilter
+) => {
+  const initialData = relevantKeys.reduce((accumulator, currentValue) => {
+    accumulator[currentValue] = {
+      values: {},
+      total: 0
+    };
+    return accumulator;
+  }, {});
+
+  const aggregateData = data.reduce((accumulator, currentValue) => {
+    const targetMet = currentValue[targetKey] === targetKeyValue;
+    relevantKeys.forEach(key => {
+      const currentKeyValue = currentValue[key];
+      accumulator[key]['total'] += 1;
+      if (valueFilter(currentKeyValue)) {
+        if (!accumulator[key]['values'][currentKeyValue]) {
+          accumulator[key]['values'][currentKeyValue] = 0;
+        }
+        if (targetMet) accumulator[key]['values'][currentKeyValue] += 1;
+      }
+    });
+    return accumulator;
+  }, initialData);
+
+  return aggregateData;
+};
+
+const saveToJSON = (data, name) => {
+  fs.writeFile(`${name}.json`, JSON.stringify(data), err => {
+    if (err) throw err;
+  });
+};
+
+module.exports = {
+  aggregateData,
+  saveToJSON
+};
